12be94aba23de673cd2a150d70800c5dd5e041c3
jomcas <jomarilatigay@live.com>
2021-01-15 23:13:04 +0800

    Add SignIn/SignUp and Loading Animation

--- lib/widgets/signIn.dart
+++ lib/widgets/signIn.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mi_card/services/auth.dart';
-import 'package:mi_card/widgets/bottomNav/bottomNav.dart';
+import 'package:mi_card/widgets/shared/loading.dart';
 import 'package:mi_card/widgets/signUp.dart';
 import 'animation/slideRight.dart';
 
@@ -11,135 +11,178 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+  String error = '';
+  bool loading = false;
+  String email = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        brightness: Brightness.light,
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: Icon(
-            Icons.arrow_back_rounded,
-            size: 30,
-            color: Colors.black,
-          ),
-        ),
-      ),
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-          child: Column(
-        children: <Widget>[
-          Container(
-            margin: const EdgeInsets.all(4.0),
-            padding: const EdgeInsets.only(top: 10),
-          ),
-          Container(
-            padding: EdgeInsets.only(bottom: 100),
-            height: 150,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("images/logo.png"),
-                    fit: BoxFit.fitHeight)),
-          ),
-          Column(
-            children: <Widget>[
-              Text(
-                "Welcome Back!",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    return loading
+        ? Loading()
+        : Scaffold(
+            appBar: AppBar(
+              elevation: 0,
+              brightness: Brightness.light,
+              backgroundColor: Colors.white,
+              leading: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(
+                  Icons.arrow_back_rounded,
+                  size: 30,
+                  color: Colors.black,
+                ),
               ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                "Login to your account",
-                style: TextStyle(fontSize: 15, color: Colors.grey[700]),
-              ),
-            ],
-          ),
-          Container(
-            margin: const EdgeInsets.all(4.0),
-            padding: const EdgeInsets.only(top: 10),
-          ),
-          Container(
-            child: Column(
+            ),
+            backgroundColor: Colors.white,
+            body: SingleChildScrollView(
+                child: Column(
               children: <Widget>[
-                SizedBox(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 40),
+                Container(
+                  margin: const EdgeInsets.all(4.0),
+                  padding: const EdgeInsets.only(top: 10),
+                ),
+                Container(
+                  padding: EdgeInsets.only(bottom: 100),
+                  height: 150,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("images/logo.png"),
+                          fit: BoxFit.fitHeight)),
+                ),
+                Column(
+                  children: <Widget>[
+                    Text(
+                      "Welcome Back!",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Login to your account",
+                      style: TextStyle(fontSize: 15, color: Colors.grey[700]),
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: const EdgeInsets.all(4.0),
+                  padding: const EdgeInsets.only(top: 10),
+                ),
+                Container(
+                  child: Form(
+                    key: _formKey,
                     child: Column(
                       children: <Widget>[
-                        inputFile(label: "Email:"),
-                        inputFile(label: "Password:", obscureText: true)
+                        SizedBox(
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 40),
+                            child: Column(
+                              children: <Widget>[
+                                inputFile(
+                                    label: "Email:",
+                                    validator: (val) =>
+                                        val.isEmpty ? 'Enter an email' : null,
+                                    onChanged: (val) {
+                                      setState(() => email = val);
+                                    }),
+                                inputFile(
+                                    label: "Password:",
+                                    validator: (val) => val.length < 6
+                                        ? 'Enter a password 6+ chars long'
+                                        : null,
+                                    obscureText: true,
+                                    onChanged: (val) {
+                                      setState(() => password = val);
+                                    }),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Divider(),
+                        SizedBox(
+                          width: 335,
+                          child: MaterialButton(
+                            minWidth: double.infinity,
+                            height: 60,
+                            onPressed: () async {
+                              // May bug na kapag mali yung login mapupunta sa welcome
+
+                              if (_formKey.currentState.validate()) {
+                                setState(() => loading = true);
+
+                                dynamic result =
+                                    await _auth.signInWithEmailAndPassword(
+                                        email, password);
+
+                                Navigator.pop(context);
+                                loading = false;
+                                if (result == null) {
+                                  setState(() {
+                                    error =
+                                        'Could not sign in with those credentials';
+                                    loading = false;
+                                  });
+                                }
+                              }
+                            },
+                            color: Color(0xff0795A8),
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                            child: Text(
+                              "Login",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                            width: 360,
+                            child: FlatButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  SlideRightRoute(page: SignUp()),
+                                );
+                              },
+                              child: Text('Dont have an account? Sign Up',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700)),
+                            )),
+                        SizedBox(
+                            width: 360,
+                            child: FlatButton(
+                              onPressed: () async {
+                                dynamic result = await _auth.signInAnon();
+                                Navigator.pop(context);
+                                if (result == null) {
+                                  print('error signing in');
+                                } else {
+                                  print('signed in');
+                                  print(result.uid);
+                                }
+                              },
+                              child: Text('Sign In Anonymously',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700)),
+                            ))
                       ],
                     ),
                   ),
-                ),
-                Divider(),
-                SizedBox(
-                  width: 335,
-                  child: MaterialButton(
-                    minWidth: double.infinity,
-                    height: 60,
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        SlideRightRoute(page: BottomNav()),
-                      );
-                    },
-                    color: Color(0xff0795A8),
-                    elevation: 0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                    width: 360,
-                    child: FlatButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          SlideRightRoute(page: SignUp()),
-                        );
-                      },
-                      child: Text('Dont have an account? Sign Up',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w700)),
-                    )),
-                SizedBox(
-                    width: 360,
-                    child: FlatButton(
-                      onPressed: () async {
-                        dynamic result = await _auth.signInAnon();
-                        if (result == null) {
-                          print('error signing in');
-                        } else {
-                          print('signed in');
-                          print(result.uid);
-                        }
-                      },
-                      child: Text('Sign In Anonymously',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w700)),
-                    ))
+                )
               ],
-            ),
-          )
-        ],
-      )),
-    );
+            )),
+          );
   }
 }
--- lib/widgets/signUp.dart
+++ lib/widgets/signUp.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mi_card/services/auth.dart';
+import 'package:mi_card/widgets/shared/loading.dart';
 import 'package:mi_card/widgets/signIn.dart';
-import 'package:mi_card/widgets/bottomNav/bottomNav.dart';
 
 import 'animation/slideRight.dart';
 
@@ -10,120 +11,153 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  final AuthService _auth = AuthService();
+
+  final _formKey = GlobalKey<FormState>();
+  bool loading = false;
+  String error = '';
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Inter'),
-      home: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          brightness: Brightness.light,
-          backgroundColor: Colors.white,
-          leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(
-              Icons.arrow_back_rounded,
-              size: 30,
-              color: Colors.black,
+    return loading
+        ? Loading()
+        : Scaffold(
+            appBar: AppBar(
+              elevation: 0,
+              brightness: Brightness.light,
+              backgroundColor: Colors.white,
+              leading: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(
+                  Icons.arrow_back_rounded,
+                  size: 30,
+                  color: Colors.black,
+                ),
+              ),
             ),
-          ),
-        ),
-        backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-            child: Column(
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.all(4.0),
-              padding: const EdgeInsets.only(top: 0),
-            ),
-            Text("Let's Get Started!",
-                style: TextStyle(
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                    letterSpacing: 2.5)),
-            Container(
-              margin: const EdgeInsets.all(4.0),
-              padding: const EdgeInsets.only(top: 10),
-            ),
-            Container(
-              child: Column(
-                children: <Widget>[
-                  SizedBox(
-                    width: 400,
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 40),
-                      child: Column(
-                        children: <Widget>[
-                          inputFile(label: "Last Name:"),
-                          inputFile(label: "First Name:"),
-                          inputFile(label: "Email:"),
-                          inputFile(label: "Password:", obscureText: true),
-                          inputFile(label: "Phone Number:"),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  SizedBox(
-                    width: 300,
-                    child: MaterialButton(
-                      minWidth: double.infinity,
-                      height: 60,
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => BottomNav()),
-                        );
-                      },
-                      color: Color(0xff0795A8),
-                      elevation: 0,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25),
-                      ),
-                      child: Text(
-                        "Register",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 18,
-                          color: Colors.white,
+            backgroundColor: Colors.white,
+            body: SingleChildScrollView(
+                child: Column(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.all(4.0),
+                  padding: const EdgeInsets.only(top: 0),
+                ),
+                Text("Let's Get Started!",
+                    style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        letterSpacing: 2.5)),
+                Container(
+                  margin: const EdgeInsets.all(4.0),
+                  padding: const EdgeInsets.only(top: 10),
+                ),
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 400,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 40),
+                          child: Form(
+                            key: _formKey,
+                            child: Column(
+                              children: <Widget>[
+                                inputFile(
+                                  label: "Last Name:",
+                                ),
+                                inputFile(label: "First Name:"),
+                                inputFile(
+                                    label: "Email:",
+                                    validator: (val) =>
+                                        val.isEmpty ? 'Enter an email' : null,
+                                    onChanged: (val) {
+                                      setState(() => email = val);
+                                    }),
+                                inputFile(
+                                    label: "Password:",
+                                    validator: (val) => val.length < 6
+                                        ? 'Enter a password 6+ chars long'
+                                        : null,
+                                    obscureText: true,
+                                    onChanged: (val) {
+                                      setState(() => password = val);
+                                    }),
+                                inputFile(label: "Phone Number:"),
+                              ],
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 0,
-                  ),
-                  SizedBox(
-                      width: 360,
-                      child: FlatButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            SlideRightRoute(page: SignIn()),
-                          );
-                        },
-                        child: Text('Alreday have an account? Sign In',
+                      SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        width: 300,
+                        child: MaterialButton(
+                          minWidth: double.infinity,
+                          height: 60,
+                          onPressed: () async {
+                            if (_formKey.currentState.validate()) {
+                              setState(() => loading = true);
+                              dynamic result =
+                                  await _auth.registerWithEmailAndPassword(
+                                      email, password);
+                              if (result == null) {
+                                setState(() {
+                                  loading = false;
+                                  error = 'Please supply a valid email';
+                                });
+                              }
+                            }
+                          },
+                          color: Color(0xff0795A8),
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          child: Text(
+                            "Register",
                             style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.w700)),
-                      )),
-                ],
-              ),
-            )
-          ],
-        )),
-      ),
-    );
+                              fontWeight: FontWeight.w600,
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 0,
+                      ),
+                      SizedBox(
+                          width: 360,
+                          child: FlatButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                SlideRightRoute(page: SignIn()),
+                              );
+                            },
+                            child: Text('Alreday have an account? Sign In',
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.w700)),
+                          )),
+                    ],
+                  ),
+                )
+              ],
+            )),
+          );
   }
 }
 
 //Text Widget
-Widget inputFile({label, obscureText = false}) {
+Widget inputFile({label, obscureText = false, onChanged, validator}) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: <Widget>[
@@ -135,7 +169,9 @@ Widget inputFile({label, obscureText = false}) {
       SizedBox(
         height: 5,
       ),
-      TextField(
+      TextFormField(
+        validator: validator,
+        onChanged: onChanged,
         obscureText: obscureText,
         decoration: InputDecoration(
           contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
